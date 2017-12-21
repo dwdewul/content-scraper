a97687137a6ee1408a3328038f07ae71daee8457
David DeWulf <dwdewul@gmail.com>
2017-12-21 11:46:56 -0600

    Fixed headers duplication issue Added error logging functionality

--- scraper.js
+++ scraper.js
@@ -14,11 +14,18 @@ if(!fs.existsSync('data')) {
     fs.mkdir('data');
 }
 
-const scrapeShirts = async () => {
+const scrapeShirts = () => {
+    // set current date using moment
+    let date = moment().format('YYYY-MM-DD');
+    // create empty pages array
+    let pages = [];
+    // loop over the possible shirt IDs
     for(let i = 101; i <= 108; i++) {
+        // scrape out the requisite info into a JS object
         scraper(url + '?id=' + i, {
             Title: {
                 selector: 'h1',
+                // Only get the t-shirt title from the h1
                 convert: (x) => x.split('$')[1].slice(3)
             },
             Price: '.price',
@@ -29,19 +36,25 @@ const scrapeShirts = async () => {
         }).then(page => {
             page.Time = moment().format('YYYY-MMM-DD, h:mm:ss');
             page.URL = url + '?id=' + i;
-            writeToCSV(page);
+            // push page onto pages array
+            pages.push(page);
+            // write pages array to the CSV
+            json2csv({ data: pages, fields: csvTitles }, (err, csv) => {
+                fs.writeFile(path.join('data', `${date}.csv`), csv, (err) => {
+                    if (err) throw err;
+                    // console.log(page);
+                });
+            });
+        }).catch(err => {
+            // On error, console.log it and write it to a file with timestamp
+            console.log('Unable to connect to Mike\'s shirts! Error: ', err);
+            let errorMessage = '\n' + moment().utc().format() + ' | ' + err;
+            fs.appendFile('scraper-error.log', errorMessage, (logErr) => {
+                if (logErr) throw err;
+            })
         });
     }
 }
 
-const writeToCSV = async (page) => {
-    let csv = json2csv({ data: page, hasCSVColumnTitle: false });
-    let date = moment().format('YYYY-MM-DD');
-
-    fs.appendFile(path.join('data', `${date}.csv`), csv, (err) => {
-        if (err) throw err;
-
-    })
-}
-
+// call function
 scrapeShirts();
